4b979600741da198bb650a3fd6d0051d65218f0d
jeremy-marple <>
2023-07-05 15:34:05 -0400

    s

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        AdBolt
+                        <b>AdBolt</b>
                     </a>
                 </div>
 
